0c494278e07cefb3f525ea9896b99a46d0d9a007
David Eliason <david@davethemaker.com>
2018-07-02 16:07:41 -0700

    slight change to passed var in route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'David' });
+  res.render('index', { title: 'MyLibrary' });
 });
 
 module.exports = router;
